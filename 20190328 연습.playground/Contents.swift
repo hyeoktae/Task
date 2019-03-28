09d5f591b8e3b8b9107e3328efb3a38a8e61b5ba
hyeoktae kwon <kht_0218@hanmail.net>
2019-03-28 10:28:39 +0900

    20190328 연습
    과제 풀이

--- 20190328 연습.playground/Contents.swift
+++ 20190328 연습.playground/Contents.swift
@@ -4,3 +4,17 @@ func test01(_ name: String, _ age: Int) {
     print("이름은: \(name), 나이는: \(age)")
 }
 test01("tass", 29)
+
+
+
+func test02(_ number: Int) -> Bool {
+    guard (number % 2) == 0 else {
+        return false
+    }
+    return true
+}
+test02(21)
+test02(20)
+
+
+
