e998091a8a5ab1838353d29d6775b9bb43690607
hyeoktae kwon <kht_0218@hanmail.net>
2019-03-28 10:54:12 +0900

    Update Contents.swift

--- 20190328 연습.playground/Contents.swift
+++ 20190328 연습.playground/Contents.swift
@@ -13,8 +13,56 @@ func test02(_ number: Int) -> Bool {
     }
     return true
 }
+
+let test03: (_ num: Int) -> Bool = {($0%2)==0}
+
 test02(21)
 test02(20)
+test03(20)
+test03(21)
+
+
+func test04(_ num1: Int, _ num2: Int = 10) -> Int {
+    return num1*num2
+}
+
+let test05:(_ num1: Int, _ num2: Int) -> Int = {$0 * $1}
+
+test04(7)
+test04(7,3)
+test05(7,2)
 
 
 
+let test06:(_ num1: Int, _ num2: Int, _ num3: Int, _ num4: Int) -> Int = {($0+$1+$2+$3)/4}
+
+test06(50,60,70,80)
+
+
+
+func test07(_ score: Any) -> Character {
+    var scoreInt: Int
+    if score is String {
+        scoreInt = Int(score as! String) ?? 0
+        return test08(scoreInt)
+    } else {
+        scoreInt = score as! Int
+        return test08(scoreInt)
+    }
+    
+}
+
+func test08(_ scoreInt: Int) -> Character{
+    switch scoreInt {
+    case 90...100:
+        return "A"
+    case 80..<90:
+        return "B"
+    case 70..<80:
+        return "C"
+    default:
+        return "F"
+    }
+}
+
+test07(99)
