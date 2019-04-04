9b42b185835211c4a1c6e7becac551887b5f91e0
hyeoktae kwon <kht_0218@hanmail.net>
2019-04-05 02:32:37 +0900

    20190405 delegate test

---
+++ EasyDelegate/EasyDelegate/노가다회사.swift
@@ -0,0 +1,34 @@
+//
+//  노가다회사.swift
+//  EasyDelegate
+//
+//  Created by hyeoktae kwon on 05/04/2019.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import UIKit
+
+protocol 노가다회사Delegate: class {
+    func 삽질잘하는()->String
+}
+
+class 노가다회사: UIViewController {
+    
+    weak var 채용공고: 노가다회사Delegate?
+    
+    @IBOutlet weak var 일급액수: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        일급액수계산()
+    }
+    
+    func 일급액수계산() {
+        일급액수.text = 채용공고?.삽질잘하는()
+        
+    }
+    
+    
+    
+}
---
+++ EasyDelegate/EasyDelegate/일터.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  EasyDelegate
+//
+//  Created by hyeoktae kwon on 05/04/2019.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import UIKit
+
+class 일터: UIViewController {
+    
+    @IBOutlet weak var 삽: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func 일급받기(_ sender: UIButton) {
+        performSegue(withIdentifier: "삽질끝", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: self)
+        guard let 회사 = segue.destination as? 노가다회사 else {
+            return
+        }
+        회사.채용공고 = self
+    }
+    
+}
+
+extension 일터: 노가다회사Delegate {
+    func 삽질잘하는() -> String {
+        guard let 삽질 = 삽.text else {
+            return "일안했냐?"
+        }
+        return 삽질
+    }
+    
+    @IBAction func unwindTo일터(_ unwindSegue: UIStoryboardSegue) {
+        삽.text = nil
+    }
+    
+}
