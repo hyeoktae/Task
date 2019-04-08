72790ceef2f74fcc3ad7df3983f0c464fbae3fee
hyeoktae kwon <kht_0218@hanmail.net>
2019-04-09 03:28:35 +0900

    20190409 로또 거의 완성함

---
+++ lotto/lotto/FristCustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  FristCustomCell.swift
+//  lotto
+//
+//  Created by hyeoktae kwon on 09/04/2019.
+//  Copyright © 2019 차수연. All rights reserved.
+//
+
+import UIKit
+
+class FirstCustomCell: UITableViewCell {
+    
+    @IBOutlet weak var num: UILabel!
+    @IBOutlet weak var origin: UILabel!
+    @IBOutlet weak var select: UILabel!
+    @IBOutlet weak var grade: UILabel!
+    
+}
