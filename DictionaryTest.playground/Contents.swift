4476213bff98ac2612aeb55eb40e24b1e1087c27
hyeoktae kwon <kht_0218@hanmail.net>
2019-04-15 00:51:17 +0900

    dictionary test

--- DictionaryTest.playground/Contents.swift
+++ DictionaryTest.playground/Contents.swift
@@ -43,3 +43,35 @@ print(car)
 //print(car1!.rawValue)
 //print(size1!)
 //print(size1!.rawValue)
+
+
+var dic = [1:"소녀시대", 2:"걸스데이", 3:"티아라"]
+
+
+dic[7] = "애프터스쿨"
+print(dic)
+
+for (key, value) in dic.sorted(by: {$0.0 < $1.0}) {
+    print("걸그룹 키와 값 -> \(key) : \(value)")
+}
+
+
+var phonebook : [String:[String]] = [:]
+
+var family : [String] = ["엄마", "아빠", "오빠"]
+phonebook["가족"] = family
+
+var friends : [String] = ["소녀시대", "걸스데이", "티아라"]
+phonebook["친구"] = friends
+
+print("전화번호부의 그룹 개수 : \(phonebook.count)")
+
+for (group, names) in phonebook {
+    print("전화번호부 그룹 이름 : \(group)")
+    print("\(group) 그룹의 이름들 : \(names)")
+}
+
+for (index, element) in phonebook.enumerated() {
+    print("index: \(index)")
+    print(element)
+}
