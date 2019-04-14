3c41a831d5b063805c5fb491ccb98ca6050192a2
hyeoktae kwon <kht_0218@hanmail.net>
2019-04-14 23:43:32 +0900

    test

--- DictionaryTest.playground/Contents.swift
+++ DictionaryTest.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
+var testArray = ["1", "2", "3"]
+
 var testDict = [String:[String]]()
+
+testDict.updateValue(["5"], forKey: "1")
+
+testDict["1"]?.append(contentsOf: testArray)
+
+print([String](testDict["1"] ?? []))
+
+for i in 0...5 {
+//    print(i)
+//    print(testDict.count )
+    testDict.updateValue([], forKey: "\(i)")
+}
+print(testDict.count)
+
+for i in testDict.keys {
+    print(i)
+}
