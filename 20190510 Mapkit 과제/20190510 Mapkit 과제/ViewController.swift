fbf2897904bf5a23fb10ba9a2e986f41cfa85f97
hyeoktae kwon <kht_0218@hanmail.net>
2019-05-12 18:53:44 +0900

    20190512 mapkit 과제 업글

--- 20190510 Mapkit 과제/20190510 Mapkit 과제/ViewController.swift
+++ 20190510 Mapkit 과제/20190510 Mapkit 과제/ViewController.swift
@@ -46,6 +46,105 @@ class ViewController: UIViewController {
     }
     
     
+    
+    
+    @objc func checkLocation() {
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(addTF.text ?? "", completionHandler: { (placeMark, error) in
+            guard let placeMark = placeMark?.first else { return }
+            self.destinations.append(placeMark.location!.coordinate)
+//            self.addAnnotation(placeMark.location?.coordinate) // 기본과제
+            self.mapView.removeOverlays(self.mapView.overlays)
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            self.addAnnotations(self.destinations)
+        })
+    }
+    
+    func addAnnotations(_ center: [CLLocationCoordinate2D]) {
+        guard !center.isEmpty else { return }
+        
+        mapView.delegate = self
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        let region = MKCoordinateRegion(center: center.last!, span: span)
+        let destinationLine = MKPolyline(coordinates: self.destinations, count: self.destinations.count)
+        
+        for (idx, value) in destinations.enumerated() {
+            let annotation: MKPointAnnotation = {
+                let a = MKPointAnnotation()
+                a.title = "\(idx+1)번째 행선지"
+                a.coordinate = value
+                return a
+            }()
+            
+            var p1 = value; p1.latitude += 0.003; p1.longitude -= 0.003
+            var p2 = value; p2.latitude += 0.003; p2.longitude += 0.003
+            var p3 = value; p3.latitude -= 0.003; p3.longitude += 0.003
+            var p4 = value; p4.latitude -= 0.003; p4.longitude -= 0.003
+            
+            let points: [CLLocationCoordinate2D] = [p1, p2, p3, p4, p1]
+            let polyLine = MKPolyline(coordinates: points, count: points.count)
+            
+            mapView.addAnnotation(annotation)
+            mapView.addOverlay(polyLine)
+        }
+        
+        mapView.addOverlay(destinationLine)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func addAnnotation(_ center: CLLocationCoordinate2D) {
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        let region = MKCoordinateRegion(center: center, span: span)
+        var p1 = center; p1.latitude += 0.003; p1.longitude -= 0.003
+        var p2 = center; p2.latitude += 0.003; p2.longitude += 0.003
+        var p3 = center; p3.latitude -= 0.003; p3.longitude += 0.003
+        var p4 = center; p4.latitude -= 0.003; p4.longitude -= 0.003
+        
+        let points: [CLLocationCoordinate2D] = [p1, p2, p3, p4, p1]
+        let polyLine = MKPolyline(coordinates: points, count: points.count)
+        let destinationLine = MKPolyline(coordinates: self.destinations, count: self.destinations.count)
+        
+        let annotation: MKPointAnnotation = {
+            let a = MKPointAnnotation()
+            a.title = "\(mapView.annotations.count+1)번째 행선지"
+            a.subtitle = "\(mapView.annotations.count+1)"
+            a.coordinate = center
+            return a
+        }()
+        
+        mapView.delegate = self
+        mapView.addOverlay(polyLine)
+        mapView.addOverlay(destinationLine)
+        mapView.addAnnotation(annotation)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
+    func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
+        let textColor = UIColor.red
+        let textFont = UIFont(name: "Helvetica Bold", size: 12)!
+        
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
+        
+        let textFontAttributes = [
+            NSAttributedString.Key.font: textFont,
+            NSAttributedString.Key.foregroundColor: textColor,
+            ] as [NSAttributedString.Key : Any]
+        image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
+        
+        let rect = CGRect(origin: point, size: image.size)
+        text.draw(in: rect, withAttributes: textFontAttributes)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    
     func autoLayout() {
         let guide = view.safeAreaLayoutGuide
         
@@ -70,42 +169,6 @@ class ViewController: UIViewController {
         mapView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
         mapView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
     }
-    
-    @objc func checkLocation() {
-        let geocoder = CLGeocoder()
-        
-        geocoder.geocodeAddressString(addTF.text ?? "", completionHandler: { (placeMark, error) in
-            guard let placeMark = placeMark?.first else { return }
-            self.destinations.append(placeMark.location!.coordinate)
-            self.addAnotation(placeMark.location!.coordinate)
-        })
-    }
-    
-    func addAnotation(_ center: CLLocationCoordinate2D) {
-        
-        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-        let region = MKCoordinateRegion(center: center, span: span)
-        var p1 = center; p1.latitude += 0.003; p1.longitude -= 0.003
-        var p2 = center; p2.latitude += 0.003; p2.longitude += 0.003
-        var p3 = center; p3.latitude -= 0.003; p3.longitude += 0.003
-        var p4 = center; p4.latitude -= 0.003; p4.longitude -= 0.003
-        
-        let points: [CLLocationCoordinate2D] = [p1, p2, p3, p4, p1]
-        let polyLine = MKPolyline(coordinates: points, count: points.count)
-        let destinationLine = MKPolyline(coordinates: self.destinations, count: self.destinations.count)
-        
-        let annotation: MKPointAnnotation = {
-            let a = MKPointAnnotation()
-            a.title = "\(mapView.annotations.count+1)번째 행선지"
-            a.coordinate = center
-            return a
-        }()
-        mapView.delegate = self
-        mapView.addOverlay(polyLine)
-        mapView.addOverlay(destinationLine)
-        mapView.addAnnotation(annotation)
-        mapView.setRegion(region, animated: true)
-    }
 }
 
 extension ViewController: MKMapViewDelegate {
@@ -118,4 +181,47 @@ extension ViewController: MKMapViewDelegate {
         }
         return MKOverlayRenderer(overlay: overlay)
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var annotationView: MKAnnotationView
+        
+        if let reusableView = mapView.dequeueReusableAnnotationView(withIdentifier: "Annotation") {
+            reusableView.annotation = annotation
+            annotationView = reusableView
+        } else {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "Annotation")
+            annotationView.canShowCallout = true
+            
+        annotationView.image = textToImage(drawText: "@", inImage: UIImage(named: "empty")!, atPoint: CGPoint(x: 0, y: 0))
+
+            let addButton: UIButton = {
+                let addButton = UIButton(type: .custom)
+                addButton.tag = 0
+                addButton.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+                addButton.setTitle("X", for: .normal)
+                addButton.setTitleColor(.red, for: .normal)
+                addButton.layer.borderWidth = 1
+                addButton.layer.borderColor = UIColor.red.cgColor
+                addButton.layer.cornerRadius = 10
+                addButton.titleLabel?.font = addButton.titleLabel?.font.withSize(16)
+                addButton.addTarget(self, action: #selector(deleteAnnotation), for: .touchUpInside)
+                return addButton
+            }()
+            
+            annotationView.rightCalloutAccessoryView = addButton
+        }
+        return annotationView
+    }
+    
+    @objc func deleteAnnotation() {
+        let idx = Int(mapView.selectedAnnotations[0].title!!.dropLast("번째 행선지".count))
+        
+        mapView.removeOverlays(mapView.overlays)
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.removeAnnotations(mapView.selectedAnnotations)
+        destinations.remove(at: idx! - 1)
+        addAnnotations(destinations)
+        
+    }
+    
 }
