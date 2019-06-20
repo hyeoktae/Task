c42f3291723e9064ff97d5f08005a252abc8045d
hyeoktae kwon <kht_0218@hanmail.net>
2019-06-21 04:09:19 +0900

    20190621 코딩 연습중

--- CoddingTest.playground/Contents.swift
+++ CoddingTest.playground/Contents.swift
@@ -1,14 +1,19 @@
 import UIKit
 
-var splitText = [Character]()
 var lastCharacter: Character? = nil
+var pipeCount = 0
+var laserCount = 0
 
 func test(_ text: String) {
     for idx in text {
-        
+        if idx == "(" || idx ==  ")" {
+        idx == lastCharacter ? (pipeCount += 1) : (laserCount += 1)
+        lastCharacter = idx
+        }
     }
     
-    print(splitText)
+    print("pipeCount: ", pipeCount/2)
+    print("laserCount: ", laserCount/2)
 }
 
-test("((()()()()()))")
+test("()((()()()(())()))()")
